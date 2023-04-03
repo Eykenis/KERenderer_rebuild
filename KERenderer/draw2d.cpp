97824d821c164ab91e99304d3f2da2b2a6ce6bb7
Eykenis <ekennis@outlook.com>
2023-04-03 10:56:51 +0800

    Homogenous clipping (fixed)

--- KERenderer/draw2d.cpp
+++ KERenderer/draw2d.cpp
@@ -1,6 +1,5 @@
 #include "draw2d.h"
 
-void drawLine(int x0, int y0, int x1, int y1, int color) {
 extern float zbuffer[WINDOW_WIDTH + 5][WINDOW_HEIGHT + 5];
 extern kmath::vec3f lightDir;
 extern kmath::vec3f lightColor;
@@ -38,26 +37,6 @@ kmath::vec3f barycentric(kmath::vec2f p, kmath::vec2f a, kmath::vec2f b, kmath::
     return kmath::vec3f(1.0f - v - w, v, w);
 }
 
-void drawTriangle(float xa, float ya, float xb, float yb, float xc, float yc, int c1, int c2, int c3) {
-    float xl = min(xa, min(xb, xc)), xr = max(xa, max(xb, xc));
-    float yd = min(ya, min(yb, yc)), yu = max(ya, max(yb, yc));
-    for (int i = xl; i <= xr; ++i) {
-        if (i < 0 || i > WINDOW_WIDTH) continue;
-        for (int j = yd; j <= yu; ++j) {
-            if (j < 0 || j > WINDOW_HEIGHT) continue;
-            kmath::vec3f interpolate = barycentric(kmath::vec2f(i, j), kmath::vec2f(xa, ya), kmath::vec2f(xb, yb), kmath::vec2f(xc, yc));
-            if (inTriangle(interpolate)) {
-                int b = interpolate.x * (c1 & 0x0000ff) + interpolate.y * (c2 & 0x0000ff) + interpolate.z * (c3 & 0x0000ff);
-                b = b & 0x0000ff;
-                int g = interpolate.x * (c1 & 0x00ff00) + interpolate.y * (c2 & 0x00ff00) + interpolate.z * (c3 & 0x00ff00);
-                g = g & 0x00ff00;
-                int r = interpolate.x * (c1 & 0xff0000) + interpolate.y * (c2 & 0xff0000) + interpolate.z * (c3 & 0xff0000);
-                r = r & 0xff0000;
-                putpixel(i, WINDOW_HEIGHT - j, r | g | b);
-            }
-        }
-    }
-}
 
 void drawpixel(unsigned char* framebuffer, int x, int y, kmath::vec3f color) {
     int idx = (y * WINDOW_WIDTH + x) * 4;
@@ -74,4 +53,15 @@ kmath::vec3f getpixel(unsigned char* buffer, int x, int y) {
     int idx = (y * WINDOW_WIDTH + x) * 4;
     for (int i = 0; i < 3; ++i) ret.v[i] = buffer[idx + i];
     return ret;
+}
+
+void cut_to_0_255(kmath::vec3f& vec) {
+    for (int i = 0; i < 3; ++i) {
+        vec.v[i] = min(vec.v[i], 255);
+        vec.v[i] = max(vec.v[i], 0);
+    }
+}
+
+float doInterpolate(kmath::vec3f interpolate, float z1, float z2, float z3) {
+    return interpolate.x * z1 + interpolate.y * z2 + interpolate.z * z3;
 }
--- KERenderer/main.cpp
+++ KERenderer/main.cpp
@@ -8,24 +8,6 @@
 #include "Shaders.h"
 #include "afterprocess.h"
 
-kmath::mat4f model, view, proj, viewport, lightSpaceMatrix;
-kmath::vec3f cameraPos(0.f, 0.f, 3.f), cameraUp(0.f, 1.f, 0.f), cameraFront(0.f, 0.f, -1.f);
-kmath::vec3f lightDir(0.717f, 0.f, 0.717f); // trans
-kmath::vec3f lightColor(255, 255, 255);
-
-bool stencil_read = 0, stencil_write = 0;
-float* zbuffer = new float[WINDOW_WIDTH * WINDOW_HEIGHT];
-
-float* shadowbuffer = new float[WINDOW_WIDTH * WINDOW_HEIGHT];
-
-unsigned char* stencilbuffer = new unsigned char[WINDOW_WIDTH * WINDOW_HEIGHT];
-unsigned char* _shadowbuffer = new unsigned char[WINDOW_WIDTH * WINDOW_HEIGHT * 4];
-unsigned char* framebuffer = new unsigned char[WINDOW_WIDTH * WINDOW_HEIGHT * 4];
-window_t* window = NULL;
-
-void clearframebuffer(unsigned char*);
-void lighttexcutting();
-
 float xPos, yPos, lastX, lastY; // mouse position
 kmath::mat4f model, view, proj, viewport, lightSpaceMatrix;
 kmath::vec3f cameraPos(0.f, 0.f, 3.f), cameraUp(0.f, 1.f, 0.f), cameraFront(0.f, 0.f, -1.f), cameraRight(1.f, 0.f, 0.f);
@@ -52,7 +34,6 @@ void mouse_callback();
 int main() {
     clock_t start, end;
     window_init(WINDOW_WIDTH, WINDOW_HEIGHT, L"KERenderer");
-    //Mesh mesh("./obj/African.obj", "./tex/african_diffuse.tga", "./tex/african_normal_tangent.tga");
     Mesh mesh2("./obj/Aiz.obj", "./tex/Aiz_diffuse.tga");
     Mesh grass("./obj/back.obj", "./tex/back.tga");
     start = clock();
@@ -65,66 +46,8 @@ int main() {
     proj = kmath::perspective(45.f, 1.f * WINDOW_WIDTH / WINDOW_HEIGHT, 0.1f, 100.0f);
     view = kmath::lookat(cameraPos, cameraFront, cameraUp);
     model = kmath::model(kmath::vec3f(0.f, 0.f, 0.f), kmath::vec3f(1.4f, 1.4f, 1.4f), kmath::vec3f(0.f, -1.2f, 0.f));
-    viewport = kmath::viewport(0, WINDOW_WIDTH, 0, WINDOW_HEIGHT);
-
-    SimpleDepthShader depthshader(&mesh);
-    BlinnShader_shadow binnshader(&mesh, 100.f);
-
-    lighttexcutting();
-
-    int fps = 0;
-    //30.f per sec
-    while (!window->is_close) {
-        fps++;
-        end = clock();
-        if (end - start > CLOCKS_PER_SEC) {
-            printf("FPS: %d\n", fps);
-            start = clock();
-            fps = 0;
-        }
-        proj = kmath::perspective(45.f, 1.f * WINDOW_WIDTH / WINDOW_HEIGHT, 0.1f, 100.0f);
-        view = kmath::lookat(cameraPos, cameraFront, cameraUp);
-        model = kmath::model(kmath::vec3f(0.f, y, 0.f), kmath::vec3f(1.f, 1.f, 1.f), kmath::vec3f(0.f, 0.f, 0.f));
-
-        clearframebuffer(framebuffer);
-
-        //cameraFront = (kmath::model(kmath::vec3f(0.f, y, 0.f), kmath::vec3f(1.0f, 1.0f, 1.0f), kmath::vec3f(0.f, 0.f, 0.f)) * kmath::vec4f(0.0f, 0.0f, -0.1f, 1.0f)).xyz;
-        depthshader.work(shadowbuffer);
-        binnshader.work(zbuffer);
-        window_draw(framebuffer);
-        msg_dispatch();
-        if (GetAsyncKeyState(VK_LEFT)) cameraPos.x += 0.05;
-        if (GetAsyncKeyState(VK_RIGHT)) cameraPos.x -= 0.05;
-        if (GetAsyncKeyState(VK_UP)) cameraPos.z -= 0.05;
-        if (GetAsyncKeyState(VK_DOWN)) cameraPos.z += 0.05;
-        if (GetAsyncKeyState(VK_F1)) y += 2;
-        if (GetAsyncKeyState(VK_F2)) y -= 2;
-        Sleep(33);
-    }
-    window_destroy();
-	return 0;
-}
-
-void clearframebuffer(unsigned char* framebuffer) {
-    for (int i = 0; i < WINDOW_HEIGHT; ++i) {
-        for (int j = 0; j < WINDOW_WIDTH; ++j) {
-            int index = (i * WINDOW_WIDTH + j) * 4;
-            framebuffer[index + 2] = 97;
-            framebuffer[index + 1] = 63;
-            framebuffer[index] = 110;
-        }
-    }
-}
-
-    end = clock();
-    printf("%fs\n", 1. * (end - start) / CLOCKS_PER_SEC);
-	system("pause");
-	return 0;
     viewport = kmath::viewport(0, 640, 0, 480);
-    //lightPos = normalize(lightPos);
 
-    //SimpleDepthShader depthshader(&mesh);
-    //BlinnShader myshader(&mesh2, 100.f);
     BlinnShader myshader(&mesh2);
 
     int fps = 0;
